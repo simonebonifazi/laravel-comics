b37905d952dc46d4d3fbce106bfbfd570c596eae
simonebonifazi <simonbonifraz@gmail.com>
2022-09-16 08:51:18 +0200

    added yield title

---
+++ resources/views/includes/footertop.blade.php
@@ -0,0 +1,53 @@
+ <div class="footer-top">
+     <div class="container d-flex">
+         <ul id="main-links" class="d-flex">
+             <li class="dc-comics">
+                 <h3> DC-COMICS</h3>
+                 <ul>
+                     <li><a href="#"> Characters</a></li>
+                     <li><a href="#"> Comics</a></li>
+                     <li><a href="#"> Movies</a></li>
+                     <li><a href="#"> TV</a></li>
+                     <li><a href="#"> Games</a></li>
+                     <li><a href="#"> Collectibles</a></li>
+                     <li><a href="#"> Videos</a></li>
+                     <li><a href="#"> Fans</a></li>
+                     <li><a href="#"> News</a></li>
+                     <li><a href="#"> Shop</a></li>
+                     <li class="shop">
+                         <h3>SHOP</h3>
+                     </li>
+                     <li><a href="#"> Shop DC </a></li>
+                     <li><a href="#"> Shop DC Collectibles </a></li>
+                 </ul>
+             </li>
+             <li class="dc">
+                 <h3>DC</h3>
+                 <ul>
+                     <li><a href="#"> Terms Of Use</a></li>
+                     <li><a href="#"> Privacy policy (New)</a></li>
+                     <li><a href="#"> Ad Choices</a></li>
+                     <li><a href="#"> Advertising</a></li>
+                     <li><a href="#"> Jobs</a></li>
+                     <li><a href="#"> Subscription</a></li>
+                     <li><a href="#"> Talent Workshop</a></li>
+                     <li><a href="#"> CPSC Certificates</a></li>
+                     <li><a href="#"> Ratings</a></li>
+                     <li><a href="#"> Shop Help</a></li>
+                     <li><a href="#"> Contact Us</a></li>
+                 </ul>
+             </li>
+             <li class="sites">
+                 <h3>SITES</h3>
+                 <ul>
+                     <li><a href="#"> DC</a></li>
+                     <li><a href="#"> MAD Magazine</a></li>
+                     <li><a href="#"> DC Kids</a></li>
+                     <li><a href="#"> DC Universe</a></li>
+                     <li><a href="#"> DC Power Visa</a></li>
+                 </ul>
+             </li>
+         </ul>
+         <div class="dc-big-logo"></div>
+     </div>
+ </div>
