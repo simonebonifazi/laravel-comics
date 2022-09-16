34413deefe6f2c312f250d6972a069a88b37ee7a
simonebonifazi <simonbonifraz@gmail.com>
2022-09-16 16:37:41 +0200

    added cards by config

--- resources/views/includes/main.blade.php
+++ resources/views/includes/main.blade.php
@@ -1,12 +1,18 @@
 <div class="container">
     <ul class="card-container d-flex">
+
+        @foreach($comics as $comic)
         <li class="card">
-            <figure><a href="#"><img
-                        src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX"
-                        alt="Action Comics"></a></figure>
-            <figcaption>Action Comics</figcaption>
+            <figure>
+                <a href="#">
+                    <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+                </a>
+            </figure>
+            <figcaption>{{ $comic['title'] }}</figcaption>
         </li>
-        <li class="card">
+        @endforeach
+
+        <!-- <li class="card">
             <figure><a href="#"><img
                         src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01"
                         alt="American Vampire 1976"></a></figure>
@@ -71,6 +77,6 @@
                         src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2019/04/CTWv1_CC_144-001_HD_5ca5299a751963.53054221.jpg?itok=ooPaoLDq"
                         alt="Catwoman"></a></figure>
             <figcaption>Catwoman</figcaption>
-        </li>
+        </li> -->
     </ul>
 </div>
